b87a2ee7afa529f92e158b0b5286ff8f38ea0aed
Tuan Nguyen Van <nguyenvantuan.itus@gmail.com>
2017-09-24 14:00:15 +0700

    Render Adobe After Effect animations

--- app/src/main/java/com/blueeagle/lottieexample/MainActivity.java
+++ app/src/main/java/com/blueeagle/lottieexample/MainActivity.java
@@ -1,13 +1,48 @@
 package com.blueeagle.lottieexample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+
+import com.airbnb.lottie.LottieAnimationView;
+import com.airbnb.lottie.LottieComposition;
 
 public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView mLottieAnimationView;
+    Button mBtnChangeAnim;
+    String[] mAnimFiles = new String[]{"empty_status.json", "letter_b_monster.json", "permission.json", "ice_cream_animation.json"};
+    int mCurrentAnim = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mLottieAnimationView = findViewById(R.id.lottie_animation_view);
+        mBtnChangeAnim = findViewById(R.id.btn_change_animation);
+
+        mBtnChangeAnim.setOnClickListener(button -> {
+            mBtnChangeAnim.setEnabled(false);
+            changeAnim();
+        });
+    }
+
+    private void changeAnim() {
+        new Handler().post(() -> {
+            String animFileName = mAnimFiles[(++mCurrentAnim % mAnimFiles.length)];
+            LottieComposition.Factory.fromAssetFileName(this, animFileName, composition -> {
+                mLottieAnimationView.setComposition(composition);
+                mLottieAnimationView.playAnimation();
+                mBtnChangeAnim.setEnabled(true);
+            });
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        mLottieAnimationView.cancelAnimation();
+        mLottieAnimationView = null;
+        super.onDestroy();
     }
 }
